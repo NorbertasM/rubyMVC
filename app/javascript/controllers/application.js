00ae4d9e4ad6be1d0bbb7f81adf2d18016f06d19
norbertasm <murauskasnorbertas@gmail.com>
2023-01-01 12:25:12 +0200

    notification and alert styles. Fix not showing alert when login or signup fails

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -9,23 +9,25 @@ window.Stimulus   = application
 document.addEventListener("DOMContentLoaded", function(){
   const item = document.getElementById('tags-container')
 
-  const children = [...item.children]
-  children.map(child => {
-    child.addEventListener('click', function() {
-      const params = new URLSearchParams(window.location.search)
-      const allTagIds = [...params.getAll("tag_id[]")]
-
-      params.delete("tag_id[]")
-
-      allTagIds.map(tagId => {
-        if (tagId !== child.id) {
-          params.append("tag_id[]", tagId)
-        }
+  if (item?.children) {
+    const children = [...item.children]
+    children.map(child => {
+      child.addEventListener('click', function() {
+        const params = new URLSearchParams(window.location.search)
+        const allTagIds = [...params.getAll("tag_id[]")]
+  
+        params.delete("tag_id[]")
+  
+        allTagIds.map(tagId => {
+          if (tagId !== child.id) {
+            params.append("tag_id[]", tagId)
+          }
+        })
+  
+        window.location.search = params
       })
-
-      window.location.search = params
     })
-  })
+  }
 }, { once: true });
 
 export { application }
