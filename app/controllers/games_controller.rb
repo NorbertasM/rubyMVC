7da457e3f716a0caefe62345307609dcc1bc51ed
norbertasm <murauskasnorbertas@gmail.com>
2023-01-11 16:28:24 +0200

    working games search

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,35 @@
 class GamesController < ApplicationController
   def index
     @page = params["page"].to_i
+    search = params["value"]
 
-    if @page == 0
-      @page = 1
+    if search
+      @games = doSearch(search)
+      @count = nil
+      @page = nil
+    else
+
+      if @page == 0
+        @page = 1
+      end
+      
+      response = HTTParty.get("http://127.0.0.1:10000/game?page=" + (@page - 1).to_s)
+
+      if response.code === 200
+        parsed = JSON.parse(response.body)
+        @games = parsed["games"]
+        @count = parsed["count"]
+      end
+
     end
-    
-    response = HTTParty.get("http://127.0.0.1:10000/game?page=" + (@page - 1).to_s)
+  end
+
+  def doSearch(value)
+    response = HTTParty.get("http://127.0.0.1:10000/search?by=game&value=" + value)
 
     if response.code === 200
-      parsed = JSON.parse(response.body)
-      @games = parsed["games"]
-      @count = parsed["count"]
+      return JSON.parse(response.body)
     end
   end
 end
+
